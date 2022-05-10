13338fc1a6afbe601e36488f338e8b60cab1feb6
Wei Zhang <z12789@gmail.com>
2022-05-10 14:12:57 -0700

    add font style on stars and size
    - show the information of origin repo on the page
    - highlight the star and size on the list

--- js/main.js
+++ js/main.js
@@ -10,17 +10,19 @@ window.addEventListener('load', () => {
   }
 });
 
-document.getElementById('form').addEventListener('submit', e => {
+let repoInfo = null;
+
+document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
   fetchData();
 });
 
 function addDarkmodeWidget() {
-  new Darkmode( { label: '🌓' } ).showWidget();
+  new Darkmode({ label: '🌓' }).showWidget();
 }
 
 function fetchData() {
-  const repo = document.getElementById('q').value.replaceAll(' ','');
+  const repo = document.getElementById('q').value.replaceAll(' ', '');
   const re = /[-_\w]+\/[-_.\w]+/;
 
   const urlRepo = getRepoFromUrl();
@@ -31,12 +33,50 @@ function fetchData() {
 
   if (re.test(repo)) {
     fetchAndShow(repo);
+    fetchAndShowOrigin(repo);
   } else {
     showMsg(
       'Invalid GitHub repository! Format is &lt;username&gt;/&lt;repo&gt;',
       'danger'
     );
   }
+}
+
+function updateOriginRepo(data) {
+  data.repoLink = `<a href="https://github.com/${data.full_name}">Link</a>`;
+  data.ownerName = `<img src="${
+    data.owner.avatar_url || 'https://avatars.githubusercontent.com/u/0?v=4'
+  }&s=48" width="24" height="24" class="mr-2 rounded-circle" />${
+    data.owner ? data.owner.login : '<strike><em>Unknown</em></strike>'
+  }`;
+
+  const rows = window.columnNamesMap.map(
+    (colNM) =>
+      '<dt class="col-sm-3">' +
+      colNM[0] +
+      '</dt><dd class="col-sm-9">' +
+      data[colNM[1]] +
+      '</dd>'
+  );
+  $('#originRepoInfo').html("<dl class='row'>" + rows.join('') + '</dl>');
+}
+
+function dataRenderer(col, data, type) {
+  if (type === 'display') {
+    if (col[1] === 'pushed_at') {
+      return moment(data).fromNow();
+    } else if (col[1] === 'size') {
+      return data == repoInfo.size
+        ? '<span style="color:red">' + data + '</span>'
+        : data;
+    } else if (col[1] === 'stargazers_count') {
+      return data > 0
+        ? '<span style="color:green;font-weight:bold">' + data + '</span>'
+        : data;
+    }
+  }
+
+  return data;
 }
 
 function updateDT(data) {
@@ -47,16 +87,19 @@ function updateDT(data) {
   const forks = [];
   for (let fork of data) {
     fork.repoLink = `<a href="https://github.com/${fork.full_name}">Link</a>`;
-    fork.ownerName = `<img src="${fork.owner.avatar_url || 'https://avatars.githubusercontent.com/u/0?v=4'}&s=48" width="24" height="24" class="mr-2 rounded-circle" />${fork.owner ? fork.owner.login : '<strike><em>Unknown</em></strike>'}`;
+    fork.ownerName = `<img src="${
+      fork.owner.avatar_url || 'https://avatars.githubusercontent.com/u/0?v=4'
+    }&s=48" width="24" height="24" class="mr-2 rounded-circle" />${
+      fork.owner ? fork.owner.login : '<strike><em>Unknown</em></strike>'
+    }`;
+
     forks.push(fork);
   }
-  const dataSet = forks.map(fork =>
-    window.columnNamesMap.map(colNM => fork[colNM[1]])
+
+  const dataSet = forks.map((fork) =>
+    window.columnNamesMap.map((colNM) => fork[colNM[1]])
   );
-  window.forkTable
-    .clear()
-    .rows.add(dataSet)
-    .draw();
+  window.forkTable.clear().rows.add(dataSet).draw();
 }
 
 function initDT() {
@@ -77,35 +120,53 @@ function initDT() {
   // Sort by stars:
   const sortColName = 'Stars';
   const sortColumnIdx = window.columnNamesMap
-    .map(pair => pair[0])
+    .map((pair) => pair[0])
     .indexOf(sortColName);
 
   // Use first index for readable column name
   // we use moment's fromNow() if we are rendering for `pushed_at`; better solution welcome
   window.forkTable = $('#forkTable').DataTable({
-    columns: window.columnNamesMap.map(colNM => {
+    columns: window.columnNamesMap.map((colNM) => {
       return {
         title: colNM[0],
-        render:
-          colNM[1] === 'pushed_at'
-            ? (data, type, _row) => {
-                if (type === 'display') {
-                  return moment(data).fromNow();
-                }
-                return data;
-              }
-            : null,
+        render: function (data, type) {
+          return dataRenderer(colNM, data, type);
+        },
       };
     }),
     order: [[sortColumnIdx, 'desc']],
     // paging: false,
-    searchBuilder:{
+    searchBuilder: {
       // all options at default
-    }
+    },
   });
   let table = window.forkTable;
   new $.fn.dataTable.SearchBuilder(table, {});
   table.searchBuilder.container().prependTo(table.table().container());
+}
+
+function fetchAndShowOrigin(repo) {
+  repo = repo.replace('https://github.com/', '');
+  repo = repo.replace('http://github.com/', '');
+  repo = repo.replace(/\.git$/, '');
+
+  fetch(`https://api.github.com/repos/${repo}`)
+    .then((response) => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
+    .then((data) => {
+      repoInfo = data;
+      updateOriginRepo(data);
+    })
+    .catch((error) => {
+      const msg =
+        error.toString().indexOf('Forbidden') >= 0
+          ? 'Error: API Rate Limit Exceeded'
+          : error;
+      showMsg(`${msg}. Additional info in console`, 'danger');
+      console.error(error);
+    });
 }
 
 function fetchAndShow(repo) {
@@ -116,14 +177,14 @@ function fetchAndShow(repo) {
   fetch(
     `https://api.github.com/repos/${repo}/forks?sort=stargazers&per_page=100`
   )
-    .then(response => {
+    .then((response) => {
       if (!response.ok) throw Error(response.statusText);
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       updateDT(data);
     })
-    .catch(error => {
+    .catch((error) => {
       const msg =
         error.toString().indexOf('Forbidden') >= 0
           ? 'Error: API Rate Limit Exceeded'
